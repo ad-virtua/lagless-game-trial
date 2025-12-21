94b163130f844ed8987f4cd187a68c14a52af204
macaron0814 <yuta_0814@outlook.jp>
2025-12-21 20:05:25 +0900

    修正

--- Assets/Script/EnemyMoveLoop.cs
+++ Assets/Script/EnemyMoveLoop.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using static EnemyParameters;
-using static UnityEditor.PlayerSettings;
 
 public class EnemyMoveLoop : MonoBehaviour
 {
